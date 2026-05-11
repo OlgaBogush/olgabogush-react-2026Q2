4d07e9e9dcbcd9bbe4cd4cd70e3f88cd5c0ab362
OlgaBogush <bohush.olga@gmail.com>
2026-05-11 12:44:08 +0300

    feat: add CardList.test.tsx

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,29 +1,29 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    transform: {
-        '^.+\\.tsx?$': [
-            'ts-jest',
-            {
-                tsconfig: 'tsconfig.app.json',
-            },
-        ],
-    },
-    collectCoverage: true,
-    coverageThreshold: {
-        global: {
-            statements: 80,
-            branches: 50,
-            functions: 50,
-            lines: 50,
-        },
-    },
-    collectCoverageFrom: [
-        'src/**/*.{js,jsx,ts,tsx}',
-        '!src/**/*.test.{js,jsx,ts,tsx}',
-        '!src/**/*.spec.{js,jsx,ts,tsx}',
-        '!src/index.{js,jsx,ts,tsx}',
-        '!src/setupTests.{js,ts}',
-        '!src/**/*.d.ts',
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.app.json',
+      },
     ],
-}
+  },
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 50,
+      functions: 50,
+      lines: 50,
+    },
+  },
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.test.{js,jsx,ts,tsx}',
+    '!src/**/*.spec.{js,jsx,ts,tsx}',
+    '!src/index.{js,jsx,ts,tsx}',
+    '!src/setupTests.{js,ts}',
+    '!src/**/*.d.ts',
+  ],
+};
--- src/components/CardsList.tsx
+++ src/components/CardsList.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Card from './Card';
 
-interface DataItem {
+export interface DataItem {
   name: string;
   url: string;
 }
